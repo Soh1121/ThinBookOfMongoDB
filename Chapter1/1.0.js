03dd2aae0d60318d5c10c42d413c02d6056e3f95
Soh1121 <satou.shg@gmail.com>
2021-12-29 16:17:18 +0900

    [add]第1章のイントロダクションを学習

---
+++ Chapter1/1.0.js
@@ -0,0 +1,7 @@
+db.unicorns.insert({name: 'Aurora', gender: 'f', weight: 450})
+
+db.unicorns.find()
+
+db.unicorns.getIndexes()
+
+db.unicorns.insert({name: 'Leto', gender: 'm', home: 'Arrakeen', worm: false})
